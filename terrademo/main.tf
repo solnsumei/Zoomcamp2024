25c2133b8d0e953caeb3ec1ed91ce2798ce7dd20
Solomon Nsumei <solnsumei@users.noreply.github.com>
2024-02-23 09:39:33 +0000

    create gcp services with terraform

---
+++ terrademo/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "data-works-415208"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "data-works-415208-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
